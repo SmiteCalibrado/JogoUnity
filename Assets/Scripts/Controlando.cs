5e137cfd8cb150405efb1f52f645dbc4cdf8e19d
SmiteCalibrado <herogamer28@gmail.com>
2018-06-17 23:26:08 -0300

    aprendendo

---
+++ Assets/Scripts/Controlando.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controlando : MonoBehaviour {
+
+    //int e float
+    int Notas;
+    int Alunos;
+    int Dinheiro;
+    int QualiAula = 1;
+    int QualiCantina = 1;
+    int QualiQuadra = 1;
+    float Felicidade = 1;
+
+    //textos
+    public Text txtNotas;
+    public Text txtDinheiro;
+    public Text txtAlunos;
+    
+
+    //botoes
+    public Button Aulas;
+    public Button Comida;
+    public Button Quadra;
+
+    //Image
+    public Image ImagemFeliz;
+
+
+    public GameObject MenuCantina;
+    public GameObject MenuAula;
+    public GameObject MenuEdF;
+
+
+    // Use this for initialization
+    void Start() {
+        MenuCantina.SetActive(false);
+        MenuAula.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update() {        
+        SetandoTexto();
+        ImagemFeliz.fillAmount = Felicidade;
+            }
+
+    public void ContandoNotas() {
+        Notas++;
+        Notas = Notas* QualiAula;
+        Felicidade = Felicidade - 0.05f;
+    }
+    public void ContandoDin()
+    {
+        Dinheiro++;    
+        Dinheiro = Dinheiro * QualiCantina;       
+    }
+    public void ContandoFelicidade()
+    {
+        Felicidade = Felicidade + 0.02f;
+        Felicidade = Felicidade * QualiQuadra;
+    }
+
+    void SetandoTexto() {
+        txtNotas.text = Notas.ToString();
+        txtDinheiro.text = Dinheiro.ToString();
+        txtAlunos.text = Alunos.ToString();
+    }
+
+    public void  AtivandoCantina(){
+        MenuCantina.SetActive(true);
+        MenuAula.SetActive(false);
+        MenuEdF.SetActive(false);
+    }
+    public void AtivandoAula()
+    {
+        MenuCantina.SetActive(false);
+        MenuEdF.SetActive(false);
+        MenuAula.SetActive(true);
+        
+    }
+    public void AtivandoQuadra()
+    {
+        MenuCantina.SetActive(false);
+        MenuEdF.SetActive(true);
+        MenuAula.SetActive(false);
+
+    }
+
+}
